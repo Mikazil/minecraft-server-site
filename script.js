2c53932819e2bcf57c39573f47718687ffd42cd5
Mika <akimivanov43@gmail.com>
2023-04-19 23:29:40 +0700

    Add rules

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+
 const onProgress = (event) => {
   const progressBar = event.target.querySelector('.progress-bar');
   const updatingBar = event.target.querySelector('.update-bar');
@@ -10,3 +11,22 @@ const onProgress = (event) => {
   }
 };
 document.querySelector('model-viewer').addEventListener('progress', onProgress);
+
+
+
+function clck() {
+  document.getElementById("btn1").style.padding = "1.7%";
+  setTimeout(function (){
+    document.getElementById("btn1").style.padding = "2%"
+  }, 150);
+}
+
+function clck2() {
+  document.getElementById("btn2").style.padding = "1.7%";
+  setTimeout(function (){
+    document.getElementById("btn2").style.padding = "2%"
+  }, 150);
+  setTimeout(function (){
+    window.open('/rules.html', '_blank')  }, 150);
+
+}
